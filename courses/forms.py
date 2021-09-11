29b7bd2c285d80b7dc8a8b7dff2bfa6d17a5508e
ps271 <53075019+ps271@users.noreply.github.com>
2021-09-11 16:34:07 +0530

    Delete courses directory

--- courses/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class CommentForm(forms.Form):
-    comment = forms.CharField(widget = forms.Textarea(
-        attrs = {
-            'class': 'form-control',
-            'placeholder': 'Leave a comment!'
-        }
-    ))
